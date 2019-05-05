8d6717bb9978632b7026eda3799c2ffeb9bd74c1
Becquet <mdevries@domoti.fr>
2019-05-05 16:36:11 +0200

    single slice OK todo add accumulator to get 620 nested slice

--- sketch/sortArrayObjShortBefore.js
+++ sketch/sortArrayObjShortBefore.js
@@ -1,32 +1,36 @@
 var array = [
-  { 'name': 's1', 'xPos': 0, 'yPos': 0 },
+  { 'name': 's1', 'xPos': 0, 'yPos': 0, 'width': 620 },
 
-    { 'name': 's2c1s1', 'xPos': 0, 'yPos': 167 },
-    { 'name': 's2c1s2', 'xPos': 0, 'yPos': 218 },
+    { 'name': 's2c1s1', 'xPos': 0, 'yPos': 167, 'width': 158 },
+    { 'name': 's2c1s2', 'xPos': 0, 'yPos': 218, 'width': 158 },
 
-    { 'name': 's2c2', 'xPos': 158, 'yPos': 167 },
+    { 'name': 's2c2', 'xPos': 158, 'yPos': 167, 'width': 254 },
 
-    { 'name': 's2c3s1', 'xPos': 412, 'yPos': 167 },
-    { 'name': 's2c3s2', 'xPos': 412, 'yPos': 218 },
-    { 'name': 's2c3s3', 'xPos': 412, 'yPos': 276 },
+    { 'name': 's2c3s1', 'xPos': 412, 'yPos': 167, 'width': 208 },
+    { 'name': 's2c3s2', 'xPos': 412, 'yPos': 218, 'width': 208 },
+    { 'name': 's2c3s3', 'xPos': 412, 'yPos': 276, 'width': 208 },
 
-    { 'name': 's3c1', 'xPos': 0, 'yPos': 330 },
-    { 'name': 's3c2', 'xPos': 342, 'yPos': 330 }
+    { 'name': 's3c1', 'xPos': 0, 'yPos': 330, 'width': 342 },
+    { 'name': 's3c2', 'xPos': 342, 'yPos': 330, 'width': 278 },
+  { 'name': 's4', 'xPos': 0, 'yPos': 672, 'width': 620 }
 ]
-// ary[idx+1]
+const newAry = []
 let unique = array.map( (item,idx,ary) => {
   if(ary[idx+1]){
-    if(item.yPos === item.xPos){
-      return 'single: '+ item.name
-    }else if(item.yPos !== ary[idx+1].yPos && item.xPos !== ary[idx+1].xPos ){
-      return item.name
+    if( item.xPos === 0 && item.width === 620  ){
+      newAry.push([{'single': item.name}])
+      ary.shift(ary[idx])
+    }else if(true){
+      console.log(idx+': ',ary[idx]);
     }
   }
-
+  else if (item.xPos === 0 && item.width === 620){
+    newAry.push([{'single': item.name}])
+    ary.shift(ary[idx])
+  }
 });
 
-
-console.log(unique);
+console.log(newAry.map(item=>item[0].single));
 
 // self -> [ 0, 167, 218, 167, 167, 218, 276, 330, 330 ]
 // result:
