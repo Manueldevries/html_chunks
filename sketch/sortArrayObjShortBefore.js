b2081897dbcff6f100cb767756c8be57ac3712e0
Becquet <mdevries@domoti.fr>
2019-05-08 15:17:59 +0200

    .vscode/launch.json debug open file

--- sketch/sortArrayObjShortBefore.js
+++ sketch/sortArrayObjShortBefore.js
@@ -14,12 +14,16 @@ var array = [
   { 'name': 's3c2', 'xPos': 342, 'yPos': 330, 'width': 278, 'height': 102 },
   { 'name': 's4', 'xPos': 0, 'yPos': 672, 'width': 620 }
 ]
+// find size first table->find yPos.min
+const minY = array.map( item => item)
+const getMin = (arg) => Math.min(...arg)
+// console.log( getMin( array.map( item => item.yPos) ) );
 
-const acc = (acc_, nextWidth) => acc_ + nextWidth
 function findZero(elt){return elt === 0}
 
 const newAry = []
 const arySameY = []
+const arySameX = []
 const xPosAry = []
 // const aryZeroX = []
 array.map(item=>newAry.push(item))
@@ -32,16 +36,20 @@ array.map( (item,idx,ary) => {
       const yPosAry = newAry.map(item => item.yPos)
       const xPosAry = newAry.map(item => item.xPos !== undefined ? item.xPos : 'close')
 
-      console.log(xPosAry.findIndex(findZero)+': '+item.name)
+      // console.log(xPosAry.findIndex(findZero)+': '+item.name)
       console.log(`[...xPosAry]: ${[...xPosAry]}`);
-      
 
       // arySameY.push([yPosAry.map(item => item === newAry[0].yPos )])
       arySameY.push( yPosAry.map(item_ => item_ === newAry[0].yPos) )
+      arySameX.push( xPosAry.map(item_ => item_ === newAry[0].xPos) )
+      const test = newAry.map( (item,idx) => arySameY[0][idx] && item.xPos === ary[idx+1].xPos ? item.name : 'false'  )
+      console.log(`test: ${test} `);
+
       newAry.shift()
     }
   }
 
 });
-// newAry.map( item=>console.log(item) )
-arySameY.map(item=>console.log(item))
+
+arySameY.map(item=>console.log( `itemY: ${item}` ))
+arySameX.map(item=>console.log( `itemX: ${item}` ))
