9f08b750f0023bc9883745bc0df11cb6791e014e
Manueldevries <48858324+Manueldevries@users.noreply.github.com>
2019-05-09 10:43:02 +0200

    show array of object comment to write algo

--- sketch/test.js
+++ sketch/test.js
@@ -11,7 +11,8 @@ var array = [
   { name: 's2c3s1', xPos: 412, yPos: 167, width: 208, height: 51 },
   { name: 's2c3s2', xPos: 412, yPos: 218, width: 208, height: 58 },
   { name: 's2c3s3', xPos: 412, yPos: 276, width: 208, height: 54 },
-
+  // x[0,0] x[158] x[412,412,412] \n x[0[y:330]]
+  // x[0[y:167],0[y:218]] x[158] x[412] \n x[0[y:330]]
   { name: 's3c1', xPos: 0, yPos: 330, width: 342, height: 102 },
   { name: 's3c2', xPos: 342, yPos: 330, width: 278, height: 102 },
 
@@ -20,6 +21,7 @@ var array = [
 // sk output var
 const newAry = array.map(clone => ({...clone}) )
 const yPosAry = array.map(item => item.yPos)
+const xPosAry = array.map(item => item.xPos)
 // building var
 const result = []
 // array sort index same value
