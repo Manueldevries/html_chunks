16493f57c32e76d535f3abb7dd025085afe8676e
Manueldevries <48858324+Manueldevries@users.noreply.github.com>
2019-05-09 09:56:53 +0200

    test.js -> dynamicly find Y [0, 167, 330, 672]

--- sketch/test.js
+++ sketch/test.js
@@ -1,13 +1,16 @@
 var array = [
   { name: 's1', xPos: 0, yPos: 0, width: 620, height: 167 },
+
   { name: 's2c1s1', xPos: 0, yPos: 167, width: 158, height: 51 },
   { name: 's2c1s2', xPos: 0, yPos: 218, width: 158, height: 112 },
   { name: 's2c2', xPos: 158, yPos: 167, width: 254, height: 163 },
   { name: 's2c3s1', xPos: 412, yPos: 167, width: 208, height: 51 },
   { name: 's2c3s2', xPos: 412, yPos: 218, width: 208, height: 58 },
   { name: 's2c3s3', xPos: 412, yPos: 276, width: 208, height: 54 },
+
   { name: 's3c1', xPos: 0, yPos: 330, width: 342, height: 102 },
   { name: 's3c2', xPos: 342, yPos: 330, width: 278, height: 102 },
+
   { name: 's4', xPos: 0, yPos: 672, width: 620 },
 ]
 
@@ -16,12 +19,12 @@ array.map(item => newAry.push(item))
 const yPosAry = array.map(item => item.yPos)
 const yPosAry_ = array.map(item => item.yPos)
 const result = []
-const aryYindex = []
 // fn get max of array
 const arrayMax = arr => arr.reduce((p, v) => (p > v ? p : v))
 const arrayMin = arr => arr.reduce((p, v) => (p < v ? p : v))
 // array sort index same value
 const arySortIdx = (ary, minY) => {
+  const aryYindex = []
   ary.filter(function(elem, index, array) {
     if (elem == minY) {
       aryYindex.push(index)
@@ -37,4 +40,4 @@ arySortIdx(yPosAry, 167)
 arySortIdx(yPosAry, 330)
 arySortIdx(yPosAry, 672)
 
-console.log( result.map(it => `${it} `) );
+console.log( result.map(it => `${it}`) );
