992853bbd200bd7475508c974a9159e3aeea7aa6
Becquet <bq_sketch_sharelib>
2019-05-15 20:45:53 +0200

    outsourceing if(w<620) add width else w=0 @show inf620

--- sketch/sortArray.js
+++ sketch/sortArray.js
@@ -7,18 +7,18 @@ var array = [
   { name: 's2c3s1', xPos: 412, yPos: 167, width: 208, height: 51 },
   { name: 's2c3s2', xPos: 412, yPos: 218, width: 208, height: 58 },
   { name: 's2c3s3', xPos: 412, yPos: 276, width: 208, height: 54 },
-  // x[0,0] x[158] x[412,412,412] \n x[0[y:330]]
-  // x[0[y:167],0[y:218]] x[158] x[412] \n x[0[y:330]]
+
   { name: 's3c1', xPos: 0, yPos: 330, width: 342, height: 102 },
   { name: 's3c2', xPos: 342, yPos: 330, width: 278, height: 102 },
 
   { name: 's4', xPos: 0, yPos: 672, width: 620 },
 ]
-// sk output var
+
 const xywPosAry = array.map(item => [item.xPos, item.yPos, item.width])
 let memo = 0
 let aryMemo = []
-
+const inf620 = (accw, curr) =>
+  accw < 620 ? ((memo += curr[2]), aryMemo.push(curr)) : (memo = 0)
 // building nested obj
 const followw = (acc, curr, i, array) => {
   if (array[i - 1] === undefined) {
@@ -37,12 +37,8 @@ const followw = (acc, curr, i, array) => {
     acc.push(curr)
   } else {
     var show_width = array[i][2]
-    if (memo < 620) {
-      memo += curr[2]
-      aryMemo.push(curr)
-    } else {
-      memo = 0
-    }
+    var show_next_width = array[i + 1][2]
+    inf620(memo, curr)
     acc.push(curr[2] === array[i + 1][2] ? [curr] : { curr: curr })
   }
   return acc
