fa1697e2bb0b8f7db4461b5d00cd55d1a60f3d36
Becquet <mdevries@domoti.fr>
2019-05-18 15:51:08 +0200

    update array -> sk data + 1 slice like master

--- sketch/sortArray.js
+++ sketch/sortArray.js
@@ -12,8 +12,15 @@ var array = [
   { name: 's3c2', xPos: 342, yPos: 330, width: 278, height: 102 },
 
   { name: 's4', xPos: 0, yPos: 672, width: 620 },
-]
 
+  { name: 's5c1s1', xPos: 0, yPos: 682, width: 158, height: 51 },
+  { name: 's5c1s2', xPos: 0, yPos: 733, width: 158, height: 112 },
+  { name: 's5c2', xPos: 158, yPos: 682, width: 254, height: 163 },
+  { name: 's5c3s1', xPos: 412, yPos: 682, width: 208, height: 51 },
+  { name: 's5c3s2', xPos: 412, yPos: 733, width: 208, height: 58 },
+  { name: 's5c3s3', xPos: 412, yPos: 791, width: 208, height: 54 },
+]
+const newAry = array.map(clone => ({ ...clone }))
 const xywPosAry = array.map(item => [item.xPos, item.yPos, item.width])
 let memo = 0
 let j    = 0
@@ -24,6 +31,7 @@ const inf620 = (accw, curr) =>
 // building nested obj
 const followw = (acc, curr, i, array) => {
   if (array[i - 1] === undefined) {
+    array[i][0] === 0 ? arySlice.push(['slice:'+newAry[i].name,'index:'+i]) : false
     acc.push( curr[2] === array[i + 1][2] ? [curr] : curr )
   } else if (curr[2] === array[i - 1][2]) {
 
@@ -37,10 +45,10 @@ const followw = (acc, curr, i, array) => {
     acc[acc.length - 1].push(curr)
 
   } else if (array[i + 1] === undefined) {
-    // console.log('end +1 = ' + array[i + 1])
-    acc.push( curr[2] === array[i + 1][2] ? [curr] : curr )
+    array[i][0] === 0 ? arySlice.push(['slice:'+newAry[i].name,'index:'+i]) : false
+    // acc.push( curr[2] === array[i + 1][2] ? [curr] : curr ) // undefined
   } else {
-    array[i][0] === 0 ? alert('new slice') : console.log(array[i][2]);
+    array[i][0] === 0 ? arySlice.push(['slice:'+newAry[i].name,'index:'+i]) : false
 
     inf620(memo, curr)
     acc.push(curr[2] === array[i + 1][2] ? [curr] : { curr: curr })
