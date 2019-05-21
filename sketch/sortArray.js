87f7a3a32b92593d47aa30577365f0640e1cea35
Becquet <bq_sketch_sharelib>
2019-05-21 20:35:54 +0200

    TOFIX js determine the depth of an array

--- sketch/sortArray.js
+++ sketch/sortArray.js
@@ -26,47 +26,73 @@ const xywPosAry = array.map(item => [item.xPos, item.yPos, item.width])
 // building nested obj
 const followw = (acc, curr, i, array) => {
   if (array[i - 1] === undefined) {
-    acc.push( curr[2] === array[i + 1][2] ? [curr] : curr )
+    acc.push(curr[2] === array[i + 1][2] ? [curr] : curr)
   } else if (curr[2] === array[i - 1][2]) {
     acc[acc.length - 1].push(curr)
-  } else if (array[i + 1] === undefined) {
-    console.log('end');
   } else {
     acc.push(curr[2] === array[i + 1][2] ? [curr] : curr)
   }
   return acc
 }
 
-const xywPosFolloww = xywPosAry.reduce(followw, []); // console.log(xywPosFolloww)
+const xywPosFolloww = xywPosAry.reduce(followw, []) // console.log(xywPosFolloww)
 
 let memo = 0
 let aryMemo = []
 let arySlice = []
-xywPosFolloww.map(i=>{
-  i[2] === 620 ?
-    (memo=0,arySlice.push(i))
-    :(
-      i[0][2] ?
-        (
-          memo < 620 ? ( memo += i[0][2], aryMemo.push(i), (memo==620 ? (memo=0,arySlice.push(aryMemo),aryMemo = []) : 'null') ) : (memo = 0, arySlice.push(aryMemo), aryMemo = [])
-        )
-        :(
-          memo < 620 ? (memo += i[2], aryMemo.push(i), (memo==620 ? (memo=0,arySlice.push(aryMemo),aryMemo = []) : 'null') ) : (memo = 0, arySlice.push(aryMemo), aryMemo = [])
-        )
-    );
+xywPosFolloww.map(i => {
+  i[2] === 620
+    ? ((memo = 0), arySlice.push(i))
+    : i[0][2]
+    ? memo < 620
+      ? ((memo += i[0][2]),
+        aryMemo.push(i),
+        memo == 620
+          ? ((memo = 0), arySlice.push(aryMemo), (aryMemo = []))
+          : 'null')
+      : ((memo = 0), arySlice.push(aryMemo), (aryMemo = []))
+    : memo < 620
+    ? ((memo += i[2]),
+      aryMemo.push(i),
+      memo == 620
+        ? ((memo = 0), arySlice.push(aryMemo), (aryMemo = []))
+        : 'null')
+    : ((memo = 0), arySlice.push(aryMemo), (aryMemo = []))
 })
 
-// console.log(`slice=
-// [[${arySlice[0]}],
-// [[[${arySlice[1][0][0]}], [${arySlice[1][0][1]}]], [${arySlice[1][1]}], [[${arySlice[1][2][0]}],[${arySlice[1][2][1]}],[${arySlice[1][2][2]}]]],
-// [[${arySlice[2][0]}],[${arySlice[2][1]}]],
-// [${arySlice[3]}],
-// [[[${arySlice[4][0][0]}],[${arySlice[4][0][1]}]],[${arySlice[4][1]}],[[${arySlice[4][2][0]}],[${arySlice[4][2][1]}],[${arySlice[4][2][2]}]]]]
-// `);
 // test profondeur tableaux
-s=(a,b,c)=>console.log(...a,b,c);
-s(arySlice,'\n_____________\narySlice.length:',arySlice.length)
+s = (a, b, c) => console.log(...a, b, c)
+s(arySlice, '\n_____________\narySlice.length:', arySlice.length)
+var start = 0
+function flatDeep(arr1) {
+  start += 1
+  return arr1.reduce(
+    (acc, val) =>
+      // Array.isArray(val) ? acc.concat(flatDeep(val)) : acc.concat(val), []
+      Array.isArray(val)
+        ? (console.log(val, ' ', start), flatDeep(val))
+        : (console.log('not array'), (start -= 1)),
+    []
+  )
+}
 
-f=a=>a[0]?Math.max(...a.map(f))+1:0
-console.log(arySlice[4][arySlice[4].length-1][0],arySlice[4][2][0], f(arySlice[4]));
+const f = a => (a[0] ? Math.max(...a.map(f)) + 1 : 0)
 
+let test1 = [0, 1, 2]
+let test2 = [[0, 1, 2], [0, 1, 2]]
+let test3 = [[0, 1, 2], [0, 1, 2], [0, 1, 2]]
+let test4 = [
+  [[0, 1, 2], [0, 1, 2], [0, 1, 2]],
+  [[[[1, 2, 3], [1, 2, 3]]]],
+  [1, 2, 3],
+  [1, 2, 3],
+]
+flatDeep(test4[0])
+console.log('test1:', f(test1))
+console.log('test2:', f(test2))
+console.log('test3:', f(test3))
+console.log('test4:', f(test4[1]))
+arySlice[4].forEach(element => {
+  console.log(f(element))
+  console.log(s(element))
+})
