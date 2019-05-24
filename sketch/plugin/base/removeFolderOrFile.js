645b6989cd665546b16a7d68e1cbe05545264030
Manueldevries <manuel@webdesignord.fr>
2019-05-24 16:12:02 +0200

    clear folder tree

--- sketch/plugin/base/removeFolderOrFile.js
+++
@@ -1,6 +0,0 @@
-// https://gist.github.com/abynim/04fd575a7e63ae2908a9
-var removeFileOrFolder = function(filePath) {
-    [[NSFileManager defaultManager] removeItemAtPath:filePath error:nil];
-}
-
-removeFileOrFolder('/Users/Manuel/Downloads/test')
--- sketch/plugin/base/sound.js
+++
@@ -1,5 +0,0 @@
-// https://github.com/turbobabr/Sketch-Plugins-Cookbook
-var filePath = "/System/Library/Sounds/Pop.aiff"
-
-var sound = NSSound.alloc().initWithContentsOfFile_byReference(filePath,true);
-sound.play();
