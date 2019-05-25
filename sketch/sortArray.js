9de2cf4e8cba9e1f66bce876ed3876490d5614b6
Becquet <bq_sketch_sharelib>
2019-05-25 19:18:46 +0200

    Start write slim

--- sketch/sortArray.js
+++ sketch/sortArray.js
@@ -1,62 +1,24 @@
-import { indent, followw, maxDepth, max } from './function.js'
-var array = [
-  { name: 's1', xPos: 0, yPos: 0, width: 620, height: 167 },
+import { indent, followw, arySlice, maxDepth, max, s } from './function.js'
 
-  { name: 's2c1s1', xPos: 0, yPos: 167, width: 158, height: 51 },
-  { name: 's2c1s2', xPos: 0, yPos: 218, width: 158, height: 112 },
-  { name: 's2c2', xPos: 158, yPos: 167, width: 254, height: 163 },
-  { name: 's2c3s1', xPos: 412, yPos: 167, width: 208, height: 51 },
-  { name: 's2c3s2', xPos: 412, yPos: 218, width: 208, height: 58 },
-  { name: 's2c3s3', xPos: 412, yPos: 276, width: 208, height: 54 },
+const creatTD = elt => `td ${elt}`
 
-  { name: 's3c1', xPos: 0, yPos: 330, width: 342, height: 102 },
-  { name: 's3c2', xPos: 342, yPos: 330, width: 278, height: 102 },
+const sortRow = (elt, depth) => {
+  // return Array.isArray(elt) ? elt.map(i => `array: ${i}`) : creatTD(elt)
+  if (max(elt) === 1) {
+    return `${indent(1)}tr${indent(2)}td ${elt}`
+  } else {
+    return elt.map(i => (max(i) > 1 ? `array:${i}` : sortRow(i)))
+  }
+}
 
-  { name: 's4', xPos: 0, yPos: 672, width: 620 },
-
-  { name: 's5c1s1', xPos: 0, yPos: 682, width: 158, height: 51 },
-  { name: 's5c1s2', xPos: 0, yPos: 733, width: 158, height: 112 },
-  { name: 's5c2', xPos: 158, yPos: 682, width: 254, height: 163 },
-  { name: 's5c3s1', xPos: 412, yPos: 682, width: 208, height: 51 },
-  { name: 's5c3s2', xPos: 412, yPos: 733, width: 208, height: 58 },
-  { name: 's5c3s3', xPos: 412, yPos: 791, width: 208, height: 54 },
-]
-
-const newAry = array.map(clone => ({ ...clone }))
-const xywPosAry = array.map(item => [item.xPos, item.yPos, item.width])
-const xywPosFolloww = xywPosAry.reduce(followw, [])
-
-let memo = 0
-let aryMemo = []
-const arySlice = []
-
-xywPosFolloww.map(i => {
-  i[2] === 620
-    ? ((memo = 0), arySlice.push(i))
-    : i[0][2]
-    ? memo < 620
-      ? ((memo += i[0][2]),
-        aryMemo.push(i),
-        memo == 620
-          ? ((memo = 0), arySlice.push(aryMemo), (aryMemo = []))
-          : 'null')
-      : ((memo = 0), arySlice.push(aryMemo), (aryMemo = []))
-    : memo < 620
-    ? ((memo += i[2]),
-      aryMemo.push(i),
-      memo == 620
-        ? ((memo = 0), arySlice.push(aryMemo), (aryMemo = []))
-        : 'null')
-    : ((memo = 0), arySlice.push(aryMemo), (aryMemo = []))
-})
-
-// test profondeur tableaux
-const s = a => console.log(...a) // s(arySlice)
-
-arySlice.forEach((element,i) => {
-  console.log( s(element) ? s(element) : 'NEXT SLICE' )
+arySlice.forEach((element, i) => {
+  console.log(s(element) ? s(element) : 'NEXT SLICE')
   console.log('max:', max(element))
-  console.log(`table.${i}${indent(1)}tr${indent(2)}td`)
+  if (i === 0) {
+    console.log(`table.${i}`)
+    console.log(`${sortRow(element)} `)
+  } else {
+    console.log(`${sortRow(element)} `)
+  }
 })
-console.log('this is the END!!!');
-// console.log(`firstline${indent(1)}secondline${indent(2)}three`)
+console.log('this is the END!!!')
