6151fc33ad6470bb2118f2e97fc9141b6f444f36
Becquet <mdevries@domoti.fr>
2019-05-31 15:19:03 +0200

    TOFIX return carriage on no nested TD

--- sketch/sortArray.js
+++ sketch/sortArray.js
@@ -5,9 +5,9 @@ const nest = (elt, cr_bool) => {
     // console.log(`elt: ${elt} -> length`, elt.length);
     elt.map( (i,x) => {
       if(x === 0){
-        return console.log(`${indent(1, cr_bool)}tr${indent(2)}td${indent(3)}table${indent(4)}tr${indent(5)}td ${i} `)
+        return console.log(`${indent(1, cr_bool)}${indent(2)}td${indent(3)}table${indent(4)}tr${indent(5)}td ${i} `)
       }else{
-        return console.log(`${indent(4,cr_bool)}tr${indent(5)}td ${i} `)
+        return console.log(`${indent(4,cr_bool)}${indent(5)}td ${i} `)
       }
     })
   }
@@ -16,17 +16,17 @@ const nest = (elt, cr_bool) => {
 const sortRow = (elt, cr_bool) => {
   // return Array.isArray(elt) ? elt.map(i => `array: ${i}`) : creatTD(elt)
   if (max(elt) === 1) {
-    return `${indent(1,cr_bool)}tr${indent(2)}td ${elt}`
+    return `${indent(1,cr_bool)}${indent(2)}td__ ${elt}`
   } else {
-    return elt.map(i => (max(i) > 1 ? nest(i, false) : sortRow(i)))
+    return elt.map((i) => (max(i) > 1 ? nest(i, false) : sortRow(i)))
   }
 }
 
 arySlice.forEach((element, i) => {
   // console.log(s(element) ? s(element) : 'NEXT SLICE')
-  // console.log('max:', max(element))
+  i === 0 ? console.log('start') : console.log(`${indent(1)}tr`);
   if (i === 0) {
-    console.log(`table.${i}`)
+    console.log(`table.${i}${indent(1,true)}tr `)
     console.log(`${sortRow(element, false)} `)
   } else {
     console.log(`${sortRow(element, false)} `)
