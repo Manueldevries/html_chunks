d874a0e2bc2c89a37c65bd2f5c227d2787cfac22
Becquet <mdevries@domoti.fr>
2019-12-07 20:02:11 +0100

    lopp for ok but map ko

--- morpion/src/index.tsx
+++ morpion/src/index.tsx
@@ -33,28 +33,39 @@ class Board extends React.Component<{}, IBoardState> {
   }
 
   render() {
-    const status = `Prochain joueur : ${this.state.xIsNext ? 'X' : 'O'}`;
+    // const winner = calculateWinner(this.state.squares);
+    // calculateWinner(this.state.squares);
+    // const status = `Prochain joueur : ${this.state.xIsNext ? 'X' : 'O'}.`;
     const winner = calculateWinner(this.state.squares);
-    console.log(winner);
-    calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = winner + ' a gagné';
+    } else {
+      console.log(winner);
+      status = 'Prochain joueur : ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
         <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+        {/* <div className="win">{winner}</div> */}
+        {console.log(calculateWinner(this.state.squares))}
+        <div className="board">
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
         </div>
       </div>
     );
@@ -89,54 +100,64 @@ ReactDOM.render(
  * https://fr.reactjs.org/tutorial/tutorial.html#completing-the-game
 */
 
-function calculateWinner(squares: (string | null)[]) {
+// function calculateWinner(squares: (string | null)[]) {
+//   const lines = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6],
+//   ];
+
+//   lines.map((line) => {
+//     const [a, b, c] = line;
+//     // let gagnant: (string | null) = squares[a]
+//     // console.log(`a: ${a}, b: ${b}, c:${c}.`);
+//     // console.log(`squares[a]: ${squares[a]}, squares[b]: ${squares[b]}, squares[c]:${squares[c]}.`);
+//     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+//       // console.log(`le joueur: ${gagnant !== null && gagnant.toUpperCase()} à gagné 🏆`);
+//       return squares[a] !== null && squares[a];
+//     }
+//     return null; // TS array-callback-return
+//   })
+//   return null;
+// }
+function calculateWinner(squares: any) {
   const lines = [
     [0, 1, 2],
-    // X, X, X
-    // 0, O, O
-    // 0, O, O
     [3, 4, 5],
-    // 0, O, O
-    // X, X, X
-    // 0, O, O
     [6, 7, 8],
-    // 0, O, O
-    // 0, O, O
-    // X, X, X
     [0, 3, 6],
-    // X, O, O
-    // X, O, O
-    // X, O, O
     [1, 4, 7],
-    // O, X, O
-    // O, X, O
-    // O, X, O
     [2, 5, 8],
-    // O, O, X
-    // O, O, X
-    // O, O, X
     [0, 4, 8],
-    // O, O, X
-    // O, X, O
-    // X, O, O
     [2, 4, 6],
-    // O, O, X
-    // O, X, O
-    // X, O, O
   ];
 
-  lines.map((line) => {
-    const [a, b, c] = line;
-    let gagnant: (string | null) = squares[a]
-    // console.log(`a: ${a}, b: ${b}, c:${c}.`);
-    // console.log(`squares[a]: ${squares[a]}, squares[b]: ${squares[b]}, squares[c]:${squares[c]}.`);
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    // console.log(`for->squares[a]${squares[a]} && squares[a]${squares[a]} === squares[b]${squares[b]} && squares[a]${squares[a]} === squares[c]${squares[c]}`);
+    // console.log(lines[i]);
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      // console.log(`le joueur: ${squares[a]} à gagné 🏆`);
-      console.log(`le joueur: ${gagnant !== null && gagnant.toUpperCase()} à gagné 🏆`);
+      // alert('gagnant?')
+      console.log(squares[a], i);
       return squares[a];
     }
-    return null; // TS array-callback-return
-  })
-  // console.log(`____________nowin____________`);
+  }
+
+  // lines.map((line, idx) => {
+  //   const [a, b, c] = line;
+  //   // console.log(`map->squares[a]${squares[a]} && squares[a]${squares[a]} === squares[b]${squares[b]} && squares[a]${squares[a]} === squares[c]${squares[c]}`);
+  //   if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+  //     // alert('gagnant?')
+  //     return squares[a];
+  //   }
+  //   console.log(squares[a], idx);
+  //   return squares[a];
+  // })
+
   return null;
 }
